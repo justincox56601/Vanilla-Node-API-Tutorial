9d6d419ab402c919905ef2ea4a99231e8c10050d
Justin <justin.cox56601@gamil.com>
2022-06-14 18:18:55 -0500

    Traversy media turtorial for vanilla Node.js REST API

---
+++ controllers/productController.js
@@ -0,0 +1,71 @@
+const Product = require('../models/productModel');
+const {getPostData} = require('../utils');
+
+/**
+ * @desc Gets all products
+ * @route GET /api/products/id
+ */
+async function getProducts(req, res){
+	try{
+		const products = await Product.findAll();
+
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end(JSON.stringify(products));
+	}catch(error){
+		console.log(error);
+	}
+}
+
+/**
+ * @desc Gets a single product by ID
+ * @route GET /api/products/id
+ */
+async function getProduct(req, res, id){
+	try{
+		const product = await Product.findById(id);
+
+		if(!product){
+			res.writeHead(404, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify({"message" : "Product Not Found"}));
+		}else{
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify(product));
+		}
+
+		
+	}catch(error){
+		console.log(error);
+	}
+}
+
+/**
+ * @desc creates a product
+ * @route POST /api/products/
+ */
+ async function createProduct(req, res){
+	try{
+		const body = await getPostData(req);
+		const {title, description, price} = JSON.parse(body);
+		
+		const product = {
+			title,
+			description,
+			price
+		}
+
+		const newProduct = await Product.create(product);
+		res.writeHead(201, {'Content-Type' : 'application/json'});
+		return res.end(JSON.stringify(newProduct));
+
+		
+		
+	}catch(error){
+		console.log(error);
+	}
+}
+
+module.exports = {
+	getProducts, 
+	getProduct, 
+	createProduct
+}
---
+++ utils.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function writeDataToFile(filename, content){
+	fs.writeFileSync(filename, JSON.stringify(content), 'utf-8', (error)=>{
+		if(error){console.log(error);}
+	});
+}
+
+function getPostData(req){
+	return new Promise((resolve, reject)=>{
+		try{
+			let body = '';
+
+			req.on('data', (chunck)=>{
+				body += chunck.toString();
+			});
+
+			req.on('end', () =>{
+				resolve(body);
+			});
+
+		}catch(error){
+			reject(error);
+		}
+	});
+}
+
+module.exports = {
+	writeDataToFile,
+	getPostData
+}
